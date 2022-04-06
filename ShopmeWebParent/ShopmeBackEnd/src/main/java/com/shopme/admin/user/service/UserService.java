d7ba1f8d7c282c0de16b8d544d9e55f80dcc1393
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2022-04-06 11:52:00 -0300

    encode user pass functionality added

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/service/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/service/UserService.java
@@ -6,6 +6,7 @@ import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,9 @@ public class UserService {
     @Autowired
     private IRoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
     }
@@ -26,6 +30,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
